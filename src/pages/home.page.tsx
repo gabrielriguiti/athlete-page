67b233b633a5a8dd6fd168b758f95c4a791efa48
Gabriel Riguiti de Oliveira <gabriel.oliveira@scheffer.agr.br>
2025-08-13 14:21:32 -0400

    :sparkles: feat: home-page

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,22 +1,43 @@
-import bgImage from '../assets/bg-homepage.jpeg';
+import bgHome1 from '../assets/bg-homepage-1.jpg';
+import bgHome2 from '../assets/bg-homepage-2.jpg';
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function HomePage() {
+
+    const navigate = useNavigate();
+
+    const [bgImageSrc, setBgImageSrc] = useState(bgHome1);
+    const [idxTabFocus, setIdxTabFocus] = useState<number>(-1);
 
     return (
         <div
             className="bg-cover bg-center h-screen flex"
-            style={{backgroundImage: `url(${bgImage})`}}
+            style={{backgroundImage: `url(${bgImageSrc})`, filter: "brightness(0.7)"}}
         >
-            <div className={"flex-grow border-l-[0.01rem] flex flex-col justify-end"}>
-                <p className={"font-montserrat font-bold text-white"}>SOBRE MIM</p>
+            <div
+                className={"flex-grow border-l-[0.01rem] flex flex-col justify-end pl-6"}
+                onMouseEnter={() => {
+                    setBgImageSrc(bgHome1);
+                    setIdxTabFocus(0);
+                }}
+                onClick={() => navigate("/about-me")}
+            >
+                <p className={"font-montserrat font-bold text-white text-2xl"}>SOBRE MIM</p>
 
-                <p className={"font-montserrat text-white"}>Saiba mais aqui</p>
+                <p className={`font-montserrat text-white ${idxTabFocus == 0 ? "mb-16" : "mb-5"}`}>Saiba mais aqui</p>
             </div>
 
-            <div className={"flex-grow border-l-[0.01rem] flex flex-col justify-end"}>
-                <p className={"font-montserrat font-bold text-white"}>CONTATO</p>
+            <div
+                className={"flex-grow border-l-[0.01rem] flex flex-col justify-end pl-6"}
+                onMouseEnter={() => {
+                    setBgImageSrc(bgHome2);
+                    setIdxTabFocus(1);
+                }}
+            >
+                <p className={"font-montserrat font-bold text-white text-2xl"}>CONTATO</p>
 
-                <p className={"font-montserrat text-white"}>Entre em contato</p>
+                <p className={`font-montserrat text-white ${idxTabFocus == 1 ? "mb-16" : "mb-5"}`}>Entre em contato</p>
             </div>
 
             <div className={"flex-grow border-l-[0.01rem]"}>
